9cf16c042c5582e0d57f1ab5e2ed285d100c63be
Eucliddd <suiyongxi@gmail.com>
2023-12-19 23:38:35 +0800

    day3 part2.

--- day3/day3.cpp
+++ day3/day3.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <array>
 #include <regex>
+#include <cmath>
 
 using namespace std;
 
@@ -58,16 +59,137 @@ int puzzle1(const array<string,dim>& map){
 
 int puzzle2(const array<string,dim>& map){
     int sum = 0;
-    regex gearRegex("\*");
+    // match '*'
+    regex gearRegex("\\*");
     for(int k = 0; k < dim; k++){
         auto& e = map[k];
         auto words_begin = sregex_iterator(e.begin(), e.end(), gearRegex);
         auto words_end = sregex_iterator();
         for(auto i = words_begin; i != words_end; i++){
             smatch match = *i;
-            
+            int count = 0;
+            int position = match.position();
+            int gear_ratio = 1;
+            // left
+            if(position > 1 && isdigit(map[k][position-1])){
+                ++count;
+                int p = position - 1;
+                int num = 0;
+                int times = 1;
+                while(p >= 0 && isdigit(map[k][p])){
+                    num += (map[k][p]-'0')*times;
+                    times *= 10;
+                    --p;
+                }
+                cout << "left: " << num << endl;
+                gear_ratio *= num;
+            }
+            // right
+            if(position + 1 < dim && isdigit(map[k][position+1])){
+                ++count;
+                int p = position + 1;
+                int num = 0;
+                while(p < dim && isdigit(map[k][p])){
+                    num = num * 10 + (map[k][p]-'0');
+                    ++p;
+                }
+                cout << "right: " << num << endl;
+                gear_ratio *= num;
+            }
+            // up
+            if(k > 0){
+                int p = position - 1;
+                int upper_num = 0;
+                int timer = 1;
+                while(p >= 0 && isdigit(map[k-1][p])){
+                    upper_num += (map[k-1][p]-'0')*timer;
+                    timer *= 10;
+                    --p;
+                }
+                bool flag = false;
+                if(isdigit(map[k-1][position])){
+                    upper_num = upper_num*10 + (map[k-1][position]-'0');
+                    flag = true;
+                }
+                int upper_num2 = 0;
+                p = position + 1;
+                while(p < dim && isdigit(map[k-1][p])){
+                    upper_num2 = upper_num2*10 + (map[k-1][p]-'0');
+                    ++p;
+                }
+                if(flag){
+                    upper_num = upper_num*pow(10, p - position - 1) + upper_num2;
+                    cout << "up: " << upper_num << endl;
+                    gear_ratio *= upper_num;
+                    count++;
+                }
+                else {
+                    if(upper_num != 0 && upper_num2 != 0){
+                        cout << "up: " << upper_num << " " << upper_num2 << endl;
+                        gear_ratio *= upper_num * upper_num2;
+                        count += 2;
+                    }
+                    else if(upper_num != 0){
+                        cout << "up: " << upper_num << endl;
+                        gear_ratio *= upper_num;
+                        count++;
+                    }
+                    else if(upper_num2 != 0){
+                        cout << "up: " << upper_num2 << endl;
+                        gear_ratio *= upper_num2;
+                        count++;
+                    }
+                }
+            }
+            // down
+            if(k + 1 < dim){
+                int p = position - 1;
+                int down_num = 0;
+                int timer = 1;
+                while(p >= 0 && isdigit(map[k+1][p])){
+                    down_num += (map[k+1][p]-'0')*timer;
+                    timer *= 10;
+                    --p;
+                }
+                bool flag = false;
+                if(isdigit(map[k+1][position])){
+                    down_num = down_num*10 + (map[k+1][position]-'0');
+                    flag = true;
+                }
+                int down_num2 = 0;
+                p = position + 1;
+                while(p < dim && isdigit(map[k+1][p])){
+                    down_num2 = down_num2*10 + (map[k+1][p]-'0');
+                    ++p;
+                }
+                if(flag){
+                    down_num = down_num*pow(10, p - position - 1) + down_num2;
+                    cout << "down: " << down_num << endl;
+                    gear_ratio *= down_num;
+                    count++;
+                }
+                else {
+                    if(down_num != 0 && down_num2 != 0){
+                        cout << "down: " << down_num << " " << down_num2 << endl;
+                        gear_ratio *= down_num * down_num2;
+                        count += 2;
+                    }
+                    else if(down_num != 0){
+                        cout << "down: " << down_num << endl;
+                        gear_ratio *= down_num;
+                        count++;
+                    }
+                    else if(down_num2 != 0){
+                        cout << "down: " << down_num2 << endl;
+                        gear_ratio *= down_num2;
+                        count++;
+                    }
+                }
+            }
+            if(count == 2) sum += gear_ratio;
         }
     }
+    return sum;
 }
 
 int main(){
@@ -79,5 +201,5 @@ int main(){
         map[i] = line;
     }
 
-    cout << puzzle1(map) << endl;
+    cout << puzzle2(map) << endl;
 }
