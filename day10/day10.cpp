3b8df78d2cd43d474dbdca19c00dc15fa7739388
Eucliddd <suiyongxi@gmail.com>
2023-12-27 18:52:30 +0800

    day10 pt2

--- day10/day10.cpp
+++ day10/day10.cpp
@@ -18,7 +18,7 @@ enum class Direction {
     RIGHT = 3
 };
 
-bool isConnected(char a, char b, Direction dir){
+inline bool isConnected(char a, char b, Direction dir){
     if(a == 'S'){
         if(dir == Direction::UP && (b == '|' || b == '7' || b == 'F')) return true;
         if(dir == Direction::DOWN && (b == '|' || b == 'J' || b == 'L')) return true;
@@ -52,12 +52,37 @@ bool isConnected(char a, char b, Direction dir){
     return false;
 }
 
+
+inline char getConnector(int s_row, int s_col, int row1, int col1, int row2, int cow2){
+    int min_col = min(col1, cow2);
+    int max_col = max(col1, cow2);
+    int min_row = min(row1, s_row);
+    int max_row = max(row1, s_row);
+    if(min_col == max_col) return '|';
+    if(min_row == max_row) return '-';
+    if(min_col < s_col){
+        if(min_row < s_row) return 'J';
+        else return '7';
+    }
+    else{
+        if(min_row < s_row) return 'L';
+        else return 'F';
+    }
+    return 'S';
+}
+
+inline bool isWall(char a){
+    if(a == '|' || a == '7' || a == 'F' ) return true;
+    return false;
+
+}
+
 int DFS(int row, int col, array<array<char, cols>, rows>& grid, array<bitset<cols>,rows>& visited, bool& found, vector<int>& path){
     if (row < 0 || row >= rows || col < 0 || col >= cols) return 0;
     if(visited[row][col]){
         if(grid[row][col] == 'S' && path.size() > 2){
             found = true;
-            return path.size()/2;
+            return path.size();
         }
         return 0;
     }
@@ -106,8 +131,54 @@ int puzzle1(array<array<char, cols>, rows>& grid) {
     array<bitset<cols>,rows> visited;
     found = false;
     vector<int> path;
-    result = DFS(row, col, grid, visited, found, path);
+    result = DFS(row, col, grid, visited, found, path)/2;
    
+    return result;
+}
+
+int puzzle2(array<array<char, cols>, rows>& grid) {
+    int result = 0;
+    int s_row = 0;
+    int s_col = 0;
+    bool found = false;
+    for(int row = 0; row < rows; row++) {
+        for(int col = 0; col < cols; col++) {
+            if (grid[row][col] == 'S')
+            {
+                s_row = row;
+                s_col = col;
+                found = true;
+                break;
+            }
+        }
+        if(found) break;
+    }
+    int row = s_row;
+    int col = s_col;
+    array<bitset<cols>,rows> visited;
+    found = false;
+    vector<int> path;
+    DFS(row, col, grid, visited, found, path);
+    grid[s_row][s_col] = getConnector(s_row, s_col, path[1]/cols, path[1]%cols, path[path.size()-1]/cols, path[path.size()-1]%cols);
+    cout << grid[s_row][s_col] << endl;
+    sort(path.begin(), path.end());
+    bool flag = false;
+    row = path[0]/cols;
+    col = path[0]%cols - 1;
+    for(auto e: path){
+        int c_r = e/cols;
+        int c_c = e%cols;
+        if(c_r != row){
+            row = c_r;
+            col = c_c - 1;
+            flag = false;
+        }
+        if(flag) result += c_c - col - 1;
+        if(isWall(grid[c_r][c_c])){
+            flag = !flag;
+        }
+        col = c_c;
+    }
     return result;
 }
 
@@ -124,5 +195,5 @@ int main(){
         }
         row++;
     }
-    cout << "Puzzle 1: " << puzzle1(grid) << endl;
+    cout << "Puzzle 2: " << puzzle2(grid) << endl;
 }
