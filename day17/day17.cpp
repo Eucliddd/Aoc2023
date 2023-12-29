067bfa4e20c3f99d5c55407faf19922ec341699c
Eucliddd <suiyongxi@gmail.com>
2023-12-29 14:11:08 +0800

    day17.

--- day17/day17.cpp
+++ day17/day17.cpp
@@ -9,93 +9,97 @@
 
 using namespace std;
 
-const int rows = 13;
-const int cols = 13;
+const int rows = 5;
+const int cols = 5;
 using grid = array<array<uint16_t,cols>,rows>;
 const int max_dist = 1000000;
 
+enum class Direction: uint8_t{
+    UP = 0,
+    DOWN = 1,
+    LEFT = 2,
+    RIGHT = 3
+};
+
+struct Node
+{
+    int x;
+    int y;
+    Direction dir;
+    int len;
+};
+
+int _next[4][2] = {{0,1},{0,-1},{-1,0},{1,0}};
+
+
 // WRONG!!!
 int dijkstra(grid& g, int start_row = 0, int start_col = 0, int end_row = rows - 1, int end_col = cols - 1){
-    vector<vector<int>> dist(rows, vector<int>(cols, max_dist));
-    vector<bitset<cols>> visited(rows, bitset<cols>(0));
-    vector<vector<int>> prev(rows, vector<int>(cols, -1));
-    dist[start_row][start_col] = 0;
-    dist[start_row+1][start_col] = g[start_row+1][start_col];
-    dist[start_row][start_col+1] = g[start_row][start_col+1];
-    prev[start_row][start_col] = start_row * cols + start_col;
+    int dist[rows][cols][4][4] = {max_dist};
+    bool visited[rows][cols][4][4] = {0};
+    Node prev[rows][cols][4][4] = {Node{-1,-1,Direction::UP,0}};
+    dist[start_row][start_col][0][1] = 0;
 
-    for(int i = 0; i < rows * cols; i++){
+    dist[start_row+1][start_col][static_cast<int>(Direction::RIGHT)][2] = g[start_row+1][start_col];
+    prev[start_row+1][start_col][static_cast<int>(Direction::RIGHT)][2] = Node{start_row,start_col,Direction::UP,1};
+
+    dist[start_row][start_col+1][static_cast<int>(Direction::DOWN)][2] = g[start_row][start_col+1];
+    prev[start_row][start_col+1][static_cast<int>(Direction::DOWN)][2] = Node{start_row,start_col,Direction::UP,1};
+    
+
+    for(int i = 0; i < rows * cols * 16; i++){
         int min_dist = INT_MAX;
-        int min_row = -1;
-        int min_col = -1;
+        Node min_node = Node{-1,-1,Direction::UP,0};
         for(int row = 0; row < rows; row++){
             for(int col = 0; col < cols; col++){
-                if(!visited[row][col] && dist[row][col] < min_dist){
-                    min_dist = dist[row][col];
-                    min_row = row;
-                    min_col = col;
+                for(int dir = 0; dir < 4; dir++){
+                    for(int len = 1; len <= 3; len++){
+                        if(!visited[row][col][dir][len] && dist[row][col][dir][len] < min_dist){
+                            min_dist = dist[row][col][dir][len];
+                            min_node = Node{row,col,static_cast<Direction>(dir),len};
+                        }
+                    }
                 }
             }
         }
-        if(min_row == -1 || min_col == -1)
+        if(min_dist == INT_MAX)
             break;
-        visited[min_row][min_col] = 1;
-        if(min_row == end_row && min_col == end_col){
-            return dist[end_row][end_col];
-        }
-
-        bool horizontal_contiguous = false;
-        bool vertical_contiguous = false;
-        if(prev[min_row][min_col] != -1){
-            int prev_row = prev[min_row][min_col] / cols;
-            int prev_col = prev[min_row][min_col] % cols;
-            if(prev[prev_row][prev_col] != -1){
-                int prev_prev_row = prev[prev_row][prev_col] / cols;
-                int prev_prev_col = prev[prev_row][prev_col] % cols;
-                if(min_row == prev_row && prev_row == prev_prev_row){
-                    horizontal_contiguous = true;
-                }
-                else if(min_col == prev_col && prev_col == prev_prev_col){
-                    vertical_contiguous = true;
-                }
-                if (prev[prev_prev_row][prev_prev_col] != -1){
-                    int prev_prev_prev_row = prev[prev_prev_row][prev_prev_col] / cols;
-                    int prev_prev_prev_col = prev[prev_prev_row][prev_prev_col] % cols;
-                    if(horizontal_contiguous && min_row != prev_prev_prev_row){
-                        horizontal_contiguous = false;
-                    }
-                    if(vertical_contiguous && min_col != prev_prev_prev_col){
-                        vertical_contiguous = false;
-                    }
+        visited[min_node.x][min_node.y][static_cast<int>(min_node.dir)][min_node.len] = 1;
+        if(min_node.x == end_row && min_node.y == end_col){
+            int min = INT_MAX;
+            for(int dir = 0; dir < 4; dir++){
+                for(int len = 1; len <= 3; len++){
+                    if(dist[end_row][end_col][dir][len] < min)
+                        min = dist[end_row][end_col][dir][len];
                 }
             }
+            return min;
         }
-
-        if(!vertical_contiguous && min_row > 0 && !visited[min_row-1][min_col] && dist[min_row-1][min_col] > min_dist + g[min_row-1][min_col]){
-            dist[min_row-1][min_col] = min_dist + g[min_row-1][min_col];
-            prev[min_row-1][min_col] = min_row * cols + min_col;
-        }
-        if(!vertical_contiguous && min_row < rows - 1 && !visited[min_row+1][min_col] && dist[min_row+1][min_col] > min_dist + g[min_row+1][min_col]){
-            dist[min_row+1][min_col] = min_dist + g[min_row+1][min_col];
-            prev[min_row+1][min_col] = min_row * cols + min_col;
-        }
-        if(!horizontal_contiguous && min_col > 0 && !visited[min_row][min_col-1] && dist[min_row][min_col-1] > min_dist + g[min_row][min_col-1]){
-            dist[min_row][min_col-1] = min_dist + g[min_row][min_col-1];
-            prev[min_row][min_col-1] = min_row * cols + min_col;
-        }
-        if(!horizontal_contiguous && min_col < cols - 1 && !visited[min_row][min_col+1] && dist[min_row][min_col+1] > min_dist + g[min_row][min_col+1]){
-            dist[min_row][min_col+1] = min_dist + g[min_row][min_col+1];
-            prev[min_row][min_col+1] = min_row * cols + min_col;
+        
+        for(int j = 0; j < 4; j++){
+            int next_x = min_node.x + _next[j][0];
+            int next_y = min_node.y + _next[j][1];
+            if(next_x < 0 || next_x >= rows || next_y < 0 || next_y >= cols)
+                continue;
+            if(next_x == min_node.x && next_y == min_node.y)
+                continue;
         }
     }
+
     return -1;
 }
 
-int _next[4][2] = {{0,1},{1,0},{0,-1},{-1,0}};
+
 
 void DFS(grid& g, int x, int y, int step, int dist, array<bitset<cols>,rows>& visited, vector<int>& path, int& min_dist){
     if(x == rows - 1 && y == cols - 1){
-        if(dist < min_dist) min_dist = dist;
+        if(dist < min_dist) {
+            min_dist = dist;
+            for(int i = 0; i < step; i++){
+                cout << path[i] << " ";
+            }
+            cout << endl;
+            cout << min_dist << endl;
+        }
         return;
     }
     for(int i = 0; i < 4; i++){
@@ -117,6 +121,8 @@ void DFS(grid& g, int x, int y, int step, int dist, array<bitset<cols>,rows>& vi
                         continue;
                 }
             }
+            if(dist + g[next_x][next_y] >= min_dist)
+                continue;
             visited[next_x][next_y] = 1;
             path[step] = next_x * cols + next_y;
             DFS(g, next_x, next_y, step + 1, dist + g[next_x][next_y], visited, path, min_dist);
